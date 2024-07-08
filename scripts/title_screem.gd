9e2840c0cfc1a80f7f05fbfbff27e59d2b6aa9da
Ronaldo Araujo Silva Junior <ronaldogamer2006@gmail.com>
2024-07-08 10:26:04 -0300

    Splash screen

--- scripts/title_screem.gd
+++ scripts/title_screem.gd
@@ -1,9 +1,9 @@
 extends Control
 
-
+@export var animation:AnimationPlayer=null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animation.play("fade_in")
 
 func _on_btn_quit_pressed():
 	get_tree().quit()
